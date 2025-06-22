e39e91aa0ec0e5bb8d595573d4913fff782e7a35
SufiyaaanShaikh <sufisk2526@gmail.com>
2025-06-22 16:06:38 +0530

    Backend API Update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5025/api';
+const API_BASE_URL = 'https://item-manager-backend.vercel.app/api';
 
 export const createItem = async (formData) => {
   try {
